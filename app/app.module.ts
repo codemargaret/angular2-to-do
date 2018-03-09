a5b563ea186f4e6c37318ea23cbc5ebb0b5c86d2
Margaret Berry <codeberry1@gmail.com>
2018-03-09 14:41:56 -0800

    fix spacing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule }      from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule }  from '@angular/forms';
 
 @NgModule({
-  imports: [ BrowserModule,
-                  FormsModule ],
+  imports: [ BrowserModule, FormsModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
