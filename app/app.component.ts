f2091212fa3d2f7c87ac1f4d0d059e04ffa4fbca
Margaret Berry <codeberry1@gmail.com>
2018-03-09 14:50:42 -0800

    add two way binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,27 +7,25 @@ import { Component } from '@angular/core';
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
     <ul>
-      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
+      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}}<button (click)="editTask(currentTask)">Edit!</button></li>
     </ul>
     <hr>
     <div>
-      <div *ngIf="selectedTask">
-        <h3>{{selectedTask.description}}</h3>
-        <p>Task Complete? {{selectedTask.done}}</p>
-        <hr>
-        <h3>Edit Task</h3>
-        <label>Enter Task Description:</label>
-        <input [(ngModel)]="selectedTask.description">
-        <label>Enter Task Priority (1-3):</label><br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
-        <button (click)="finishedEditing()">Done</button>
-      </div>
+      <h3>{{selectedTask.description}}</h3>
+      <p>Task Complete? {{selectedTask.done}}</p>
+      <h3>Edit Task</h3>
+      <label>Enter Task Description:</label>
+      <input [(ngModel)]="selectedTask.description">
+      <label>Enter Task Priority (1-3):</label>
+      <br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
     </div>
   </div>
-    `
-  })
+  `
+})
+
 export class AppComponent {
   currentFocus: string = 'Angular Homework';
   currentTime = new Date();
@@ -39,13 +37,10 @@ export class AppComponent {
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
-  selectedTask = null;
+  selectedTask: Task = this.tasks[0];
+
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
-  }
-
-  finishedEditing() {
-    this.selectedTask = null;
   }
 
   isDone(clickedTask: Task) {
@@ -57,19 +52,17 @@ export class AppComponent {
   }
 
   priorityColor(currentTask){
-  if (currentTask.priority === 3){
-    return "bg-danger";
-  } else if (currentTask.priority === 2) {
-    return  "bg-warning";
-  } else {
-    return "bg-info";
+    if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
   }
 }
-}
-
-
 
 export class Task {
   public done: boolean = false;
-  constructor(public description: string, public priority: number) {   }
+  constructor(public description: string, public priority: number) { }
 }
